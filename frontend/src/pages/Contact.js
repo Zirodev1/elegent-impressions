e4df0fffd67390b9c0315f88f85b0341b06889c0
Lee Acevedo <91152882+Zirodev1@users.noreply.github.com>
2024-08-18 12:53:31 -0500

    Fixing contact layout

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -3,55 +3,48 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
-  return (
-    <div className="container contact-container">
-      <div>
-        
-      </div>
-      <div className="row">
+  return <>
+ <div className="contact-image">
+  <div className="contact-text">
+      <h1>Contact</h1>
+  </div>
+</div>
+
+<div className="container contact-container">
+    <div className="row">
         <div className="col-md-12 text-start">
-          <h2>Contact Us</h2>
-          <hr className="line line-hr-left" />
+            <h2>Contact Us</h2>
+            <hr className="line line-hr-left" />
         </div>
-      </div>
-      <div className="row">
-        <div className="col-md-12">
-          <div className="row">
-            <div className="col-md-6">
-              <p>
-                Thank you so much for reaching out to us - we are excited to
-                connect with you! Tell us about you. If you do not receive
-                response within 24 hours, please contact us directly email or
-                phone.
-              </p>
-              <p>
-                <span className="contacts">Phone:</span> (877)303-8368
-              </p>
-              <p>
-                <span className="contacts">eMail:</span> email@email.com
-              </p>
-              <p>
-                <span className="contacts">Address:</span> Raleigh, NC
-              </p>
-            </div>
-            <div className="col-md-6 contact-form">
-              <h3 className="">We look forward to being in touch!</h3>
-              <form className="row row-gap-3">
-                <input className="col-md-6 contact-inputs" type="text" placeholder="Full Name" />
-                <input className="col-md-6 contact-inputs" type="text" placeholder="Email" />
-                <input className="col-md-6 contact-inputs" type="text" placeholder="Phone" />
-                <input className="col-md-6 contact-inputs" type="text" placeholder="Service Type" />
-                <textarea className="contact-inputs" type="text" placeholder="Message" />
-                <button className="contact-form-btn" type="submite">
-                  Send
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  );
+    <div className="row">
+        <div className="col-md-12">
+            <div className="row">
+                <div className="col-md-6 contact-info">
+                    <p>
+                        Thank you so much for reaching out to us - we are excited to connect with you! Tell us about you. If you do not receive a response within 24 hours, please contact us directly via email or phone.
+                    </p>
+                    <p><span className="contacts">Phone:</span> (877)303-8368</p>
+                    <p><span className="contacts">eMail:</span> email@email.com</p>
+                    <p><span className="contacts">Address:</span> Raleigh, NC</p>
+                </div>
+                <div className="col-md-6 contact-form">
+                    <h3>We look forward to being in touch!</h3>
+                    <form className="row row-gap-3">
+                        <input className="col-md-5 contact-inputs" type="text" placeholder="Full Name" />
+                        <input className="col-md-5 contact-inputs" type="text" placeholder="Email" />
+                        <input className="col-md-5 contact-inputs" type="text" placeholder="Phone" />
+                        <input className="col-md-5 contact-inputs" type="text" placeholder="Service Type" />
+                        <textarea className="contact-inputs" type="text" placeholder="Message"></textarea>
+                        <button className="col-md-2 contact-form-btn" type="submit">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+  </>
 };
 
 export default Contact;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ImageSlider from "../components/ImageSlider";
+// import ImageSlider from "../components/ImageSlider";
 import Newsletter from "../components/Newsletter";
 import RecentPosts from "../components/RecentPost";
 
@@ -7,20 +7,9 @@ import './Home.css'
 
 const Home = () => {
 
-    async function fetchPost() {
-        try {
-            const response = await fetch("https://eieventanddesigns.com/wp-json/wp/v2/posts")
-            const data = await response.json()
-            console.log(data)
-        } catch (error) {
-            console.error('Error fecting posts: ', error)
-        }
-    }
-
-    fetchPost()
     return (
         <div>
-            <ImageSlider/>
+            {/* <ImageSlider/> */}
             <div className="moto bg-light container-fluid d-flex justify-content-center align-items-center">
                 <h1>Making your event a memorable one…leaving lasting great impressions.</h1>
             </div>
