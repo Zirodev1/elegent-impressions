e6003a52da31ea2a6d4a1b6de2fd601acad257e6
Lee Acevedo <91152882+Zirodev1@users.noreply.github.com>
2024-09-12 10:57:15 -0500

    added funtionality to the sign up page

--- frontend/src/pages/admin/SignUp.js
+++ frontend/src/pages/admin/SignUp.js
@@ -1,38 +1,119 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import './SignUp.css'
+
+import "./SignUp.css";
 
 const SignUp = () => {
-  return (
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    <div>
-      <form className='signup-form'>
-        <div>
-          <Label value='Username' />
-          <TextInput type='text' placeholder='UserName' id='username' />
+  function handleChange(e) {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()});
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all fields.');
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false){
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="">
+      <div className="">
+        {/* left */}
+        <div className="">
+          <Link to="/" className="">
+            <span className="">Elegent Impressions</span>
+            Blog
+          </Link>
+          <p className="">
+            This is a demo project. You can sign up with your email and password
+            or with Google.
+          </p>
         </div>
-        <div>
-          <Label value='Email' />
-          <TextInput type='text' placeholder='Email' id='email' />
+
+        <div className="">
+          <form className="" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Username" />
+              <TextInput
+                type="text"
+                placeholder="UserName"
+                id="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="email@example.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Password" />
+                <TextInput type="password"
+                placeholder="Password" 
+                id="password" 
+                onChange={handleChange}
+                />
+            </div>
+            <Button type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner className="spinner"/>
+                    <span>Loading...</span>
+                  </>
+                ) : (
+                  'Sign Up'
+                )
+              }
+            </Button>
+          </form>
+          <div className="">
+            <span>Have an account?</span>
+            <Link to="/sign-in">Sing In</Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert>
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
-        <div>
-          <Label value='Password' />
-          <TextInput type='text' placeholder='Password' id='password' />
-        </div>
-        <Button type='submit'>
-          Sign Up
-        </Button>
-      </form>
-      <div>
-        <span>Have an account?</span>
-        <Link to='/sign-in'>
-          Sing In
-        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignUp;
