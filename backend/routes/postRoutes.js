fc67ab5d357cd91a2217c8a0f21ad4c44d3350be
Lee Acevedo <91152882+Zirodev1@users.noreply.github.com>
2024-09-17 13:31:57 -0500

    created fetch post api

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createPost } from '../controllers/postController.js';
+import { createPost, getposts } from '../controllers/postController.js';
 import { verifyToken} from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
+router.get('/getposts', getposts);
 
 export default router;
 
