0131e82f9492d0baa667630a884727611acd405f
Lee Acevedo <91152882+Zirodev1@users.noreply.github.com>
2024-09-17 21:19:32 -0500

    added delete func for post

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createPost, getposts } from '../controllers/postController.js';
+import { createPost, deletepost, getposts } from '../controllers/postController.js';
 import { verifyToken} from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
 router.get('/getposts', getposts);
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 
 export default router;
 
